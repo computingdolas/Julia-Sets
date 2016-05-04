dcf29efa0bb4665208ec2d10bebbcde77d6f7fc7
Parikshit Upadhyaya <parik.iitg@gmail.com>
2016-05-04 18:31:55 +0200

    Update juliaopencl.cpp

--- juliaopencl.cpp
+++ juliaopencl.cpp
@@ -1,6 +1,8 @@
 /*Assignment 1: OpenCL code to generate Julia sets(2048*2048 pixels)*/
 
 #include <iostream>
+#define __CL_ENABLE_EXCEPTIONS
+#define CL_USE_DEPRECATED_OPENCL_1_1_APIS
 #include <CL/cl.hpp>
 #include <vector>
 #include <cassert>
@@ -9,12 +11,20 @@
 
 #define PIXELRES 2048
 
+//time function
+double getSeconds()
+{
+    struct timeval tp ;
+    gettimeofday(&tp, NULL);
+    return ((double)tp.tv_sec + (double)tp.tv_usec * 1e-6) ;
+}
+
 std::string ker_code = "\n"\
-"__kernel void juliaImage(unsigned char *color_bit_device, long long N, const double mesh, const double theshold)\n"\
+"__kernel void juliaImage(__global unsigned char *color_bit_device, __global unsigned int *N, __global double *mesh, __global double *threshold)\n"\
 "{																				   \n"\
-" 	 long long idx = get_global_id(0);											   \n"\
-"    const double x = (mesh * (idx % N)) - 2.0 ;                                   \n"\
-"    const double y = (mesh * (idx / N)) - 2.0 ;                                   \n"\
+" 	 unsigned int idx = get_global_id(0);											   \n"\
+"    double real = (mesh[0] * (idx % N[0])) - 2.0 ;                                   \n"\
+"    double imag = (mesh[0] * (idx / N[0])) - 2.0 ;                                   \n"\
 "    double temp_real = 0.0 ; 			       									   \n"\
 "    double temp_imag = 0.0 ; 													   \n"\
 "    const double c_real = -0.4 ; 												   \n"\
@@ -24,7 +34,7 @@ std::string ker_code = "\n"\
 "																				   \n"\
 "																				   \n"\
 "    unsigned int itr = 0;													       \n"\
-"    while(modulus < threshold){                                                   \n"\
+"    while(modulus < threshold[0]){                                                   \n"\
 "	                                                                               \n"\
 "      temp_real = (real * real) - (imag * imag) ;                                 \n"\
 "      temp_imag = (2 * real * imag) ;                                             \n"\
@@ -40,17 +50,16 @@ std::string ker_code = "\n"\
 "																				   \n"\
 "	   modulus = sqrt( (real * real) + (imag * imag) ) ; 						   \n"\
 "	   itr++;                                                                      \n"\
-"    }                                                                             \n"\  
+"    }                                                                             \n"\
 "    														                       \n"\
-"	unsigned int numIter_ = itr*10 +10000;										   \n"\
-"    unsigned i = idx % N ;														   \n"\
-"    unsigned j = idx / N ;														   \n"\
+"	unsigned int numIter_ = itr*10 +10000;									   \n"\
+"    unsigned i = idx % N[0] ;													   \n"\
+"    unsigned j = idx / N[0] ;													   \n"\
 "																				   \n"\
-"    color_bit_device[4*N*j+4*i+3] = (numIter_  & 255);							   \n"\
-"    color_bit_device[4*N*j+4*i+2] = (numIter_ >> 8) & 255;						   \n"\
-"    color_bit_device[4*N*j+4*i+1] = (numIter_ >> 16) & 255;					   \n"\
-"    color_bit_device[4*N*j+4*i+0] = (numIter_ >> 24) & 255;					   \n"\
-"                                            									   \n"\
+"    color_bit_device[4*N[0]*j+4*i+3] = (numIter_  & 255);							   \n"\
+"    color_bit_device[4*N[0]*j+4*i+2] = (numIter_ >> 8) & 255;						   \n"\
+"    color_bit_device[4*N[0]*j+4*i+1] = (numIter_ >> 16) & 255;					   \n"\
+"    color_bit_device[4*N[0]*j+4*i+0] = (numIter_ >> 24) & 255;					   \n"\
 "}                                                                                 \n";
 
 //Encode from raw pixels to disk with a single function call
@@ -65,90 +74,98 @@ void encodeImage(const char* filename, std::vector<unsigned char>& image, unsign
 }
 
 int main(int argc,char *argv[]){
-	if(argc < 2){
-		std::cerr<<"Number of arguements to program is less than expected"<<std::endl;
-	}
+    if(argc < 2){
+        std::cerr<<"Number of arguements to program is less than expected"<<std::endl;
+    }
 
-	int items_per_group = std::stoi(argv[1]);
+    int items_per_group = std::stoi(argv[1]);
 
-	std::vector<cl::Platform> platforms;
-	cl::Platform def_platform;
+    std::vector<cl::Platform> platforms;
+    cl::Platform def_platform;
 
-	//Number of pixels for the picture
-	long long numPixels1D = PIXELRES;
-	long long numPixels = numPixels1D*numPixels1D;
+    //Number of pixels for the picture
+    unsigned int numPixels1D = PIXELRES;
+    unsigned int numPixels = numPixels1D*numPixels1D;
 
-	int num_groups = numPixels/items_per_group;
+    int num_groups = numPixels/items_per_group;
 
-	const double mesh = 4.0/(static_cast<double>(PIXELRES));
-	const double threshold = 500.0;
+    const double mesh = 4.0/(static_cast<double>(PIXELRES));
+    const double threshold = 500.0;
 
-	std::vector<unsigned char> color_bit_host(numPixels*4,0);
+    std::vector<unsigned char> color_bit_host(numPixels*4,55);
 
-	//Get available platforms
-	cl::Platform::get(&platforms);
+    //Get available platforms
+    cl::Platform::get(&platforms);
 
-	//Select default platform
-	if(platforms.size() == 0){
-		std::cout<<"No OpenCL platforms found. Please check installation"<<std::endl;
-		exit(1);
-	}
-	else{
-		def_platform = platforms[0];
-		std::cout<<"Using platform: "<<def_platform.getInfo<CL_PLATFORM_NAME>()<<std::endl;
-	}
+    //Select default platform
+    if(platforms.size() == 0){
+        std::cout<<"No OpenCL platforms found. Please check installation"<<std::endl;
+        exit(1);
+    }
+    else{
+        def_platform = platforms[0];
+        std::cout<<"Using platform: "<<def_platform.getInfo<CL_PLATFORM_NAME>()<<std::endl;
+    }
 
-	std::vector<cl::Device> devices;
-	std::vector<cl::Device> used_device;
-	cl::Device def_device;
-	
-	//Get available devices 
-	def_platform.getDevices(CL_DEVICE_TYPE_ALL,&devices);
+    std::vector<cl::Device> devices;
+    std::vector<cl::Device> used_device;
+    cl::Device def_device;
 
-	//Select default device
-	if(devices.size() == 0){
-		std::cout<<"No devices found"<<std::endl;
-		exit(1);
-	}
-	else{
-		def_device = devices[0];
-		used_device.push_back(def_device);
-		//std::cout<<"Using device: "<<def_device.getInfo<CL_DEVICE_NAME>()<<std::endl;
-	}
+    //Get available devices
+    def_platform.getDevices(CL_DEVICE_TYPE_ALL,&devices);
 
-	//Create context with the devices
-	cl::Context context(used_device);
+    //Select default device
+    if(devices.size() == 0){
+        std::cout<<"No devices found"<<std::endl;
+        exit(1);
+    }
+    else{
+        def_device = devices[0];
+        used_device.push_back(def_device);
+        //std::cout<<"Using device: "<<def_device.getInfo<CL_DEVICE_NAME>()<<std::endl;
+    }
 
-	cl::Program::Sources sources;
-	sources.push_back({ker_code.c_str(),ker_code.length()});
+    //Create context with the devices
+    cl::Context context(used_device);
 
-	cl::Program program(context,sources);
-	if(program.build(used_device)!=CL_SUCCESS){
+    cl::Program::Sources sources;
+    sources.push_back({ker_code.c_str(),ker_code.length()});
+
+    cl::Program program(context,sources);
+    if(program.build(used_device)!=CL_SUCCESS){
         std::cout<<" Error building: "<<program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(def_device)<<"\n";
         exit(1);
     }
 
     //Create buffers on device(Equivalent to cudaMalloc)
     cl::Buffer color_bit_device(context,CL_MEM_READ_WRITE,sizeof(unsigned char)*numPixels*4);
+    cl::Buffer numPixels1D_device(context,CL_MEM_READ_WRITE,sizeof(unsigned int));
+    cl::Buffer mesh_device(context,CL_MEM_READ_WRITE,sizeof(double));
+    cl::Buffer threshold_device(context,CL_MEM_READ_WRITE,sizeof(double));
 
     //Create command queue
     cl::CommandQueue queue(context,def_device);
 
+    //Copying data to the buffers
+    queue.enqueueWriteBuffer(mesh_device,CL_TRUE,0,sizeof(double),&mesh);
+    queue.enqueueWriteBuffer(numPixels1D_device,CL_TRUE,0,sizeof(unsigned int),&numPixels1D);
+    queue.enqueueWriteBuffer(threshold_device,CL_TRUE,0,sizeof(double),&threshold);
+
     //Create kernel object
     cl::Kernel kernel_julia=cl::Kernel(program,"juliaImage");
     kernel_julia.setArg(0,color_bit_device);
-    kernel_julia.setArg(1,numPixels1D);
-    kernel_julia.setArg(2,mesh);
-    kernel_julia.setArg(3,threshold);
+    kernel_julia.setArg(1,numPixels1D_device);
+    kernel_julia.setArg(2,mesh_device);
+    kernel_julia.setArg(3,threshold_device);
     queue.enqueueNDRangeKernel(kernel_julia,cl::NullRange,cl::NDRange(numPixels),cl::NDRange(items_per_group));
+
+    //Copy the color bits back to host
+    queue.enqueueReadBuffer(color_bit_device,CL_TRUE,0,sizeof(unsigned char)*numPixels*4,&color_bit_host[0]);
+
     queue.finish();
-	
 
-
-    //Copy the color bits back
-    queue.enqueueReadBuffer(color_bit_device,CL_TRUE,0,sizeof(unsigned char)*PIXELRES,&color_bit_host[0]);
-	
-	//Encode image to juliaOpenCl.png
-	encodeImage("juliaOpenCl.png", color_bit_host, PIXELRES, PIXELRES); 
-	return 0;
+    //Encode image to juliaOpenCl.png
+    encodeImage("juliaOpenCl.png", color_bit_host, PIXELRES, PIXELRES);
+    return 0;
 }
+
